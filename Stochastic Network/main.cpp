75ef0467556c40b07be9e5e51c471f878f1603f2
RichardGodfreyson <richard.hu01@gmail.com>
2019-02-21 01:03:57 +0700

    calculate possible connectivities

--- Stochastic Network/main.cpp
+++ Stochastic Network/main.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 const int NUM_NODES = 1000;
-const int NUM_TRIALS = 100;
+const int NUM_TRIALS = 10;
+const int MAX_CONNECT = NUM_NODES / 2;
 
 double *clustering_coefficient = (double *)calloc(NUM_NODES, sizeof(double));
 
@@ -42,63 +43,65 @@ int main()
 	node_info.open("nodes.dat");
 	general_info.open("average.dat");
 
-	int connectivity = 50;
-	int num_sides = NUM_NODES * connectivity;
-	//initialize randomizer
-	srand(time(NULL));
-	//create the random graph
-	for (int t = 0; t < NUM_TRIALS; ++t)
+	for (int connectivity = 1; connectivity <= MAX_CONNECT; ++connectivity)
 	{
-		int **graph = MatrixAllocation(NUM_NODES, NUM_NODES);
-		for (int i = 0; i < num_sides; i++)
+		int num_sides = NUM_NODES * connectivity;
+		for (int t = 0; t < NUM_TRIALS; ++t)
 		{
-			int node1 = rand() % NUM_NODES;
-			int node2 = rand() % NUM_NODES;
-			graph[node1][node2] = 1;
-			graph[node2][node1] = 1;
-		}
+			//initialize randomizer
+			srand(time(NULL));
 
-		//calculate clustering coefficient for the nodes
-		double average = 0.0;
-		for (int i = 0; i < NUM_NODES; ++i)
-		{
-			double count = 0.0;
-			double neighbors = 0.0;
-
-			for (int j = 0; j < NUM_NODES; j++)
+			//create the random graph
+			int **graph = MatrixAllocation(NUM_NODES, NUM_NODES);
+			for (int i = 0; i < num_sides; i++)
 			{
-				if (graph[i][j] == 1)
+				int node1 = rand() % NUM_NODES;
+				int node2 = rand() % NUM_NODES;
+				graph[node1][node2] = 1;
+				graph[node2][node1] = 1;
+			}
+
+			//calculate clustering coefficient for the nodes
+			double average = 0.0;
+			for (int i = 0; i < NUM_NODES; ++i)
+			{
+				double count = 0.0;
+				double neighbors = 0.0;
+
+				for (int j = 0; j < NUM_NODES; j++)
 				{
-					neighbors += 1.0;
-					for (int k = 0; k < NUM_NODES; k++)
+					if (graph[i][j] == 1)
 					{
-						if (graph[j][k] == 1)
+						neighbors += 1.0;
+						for (int k = 0; k < NUM_NODES; k++)
 						{
-							count += 2.0;
+							if (graph[j][k] == 1)
+							{
+								count += 2.0;
+							}
 						}
 					}
 				}
+				if (neighbors < 2)
+				{
+					clustering_coefficient[i] = 0;
+					continue;
+				}
+				double full_edges = neighbors * (neighbors - 1);
+				clustering_coefficient[i] = count / full_edges;
+				average += clustering_coefficient[i] / NUM_NODES;
 			}
-			if (neighbors < 2)
+			//output for clustering coefficient
+			node_info << connectivity << endl;
+			for (int i = 0; i < NUM_NODES; i++)
 			{
-				clustering_coefficient[i] = 0;
-				continue;
+				node_info << clustering_coefficient[i] << " ";
 			}
-			double full_edges = neighbors * (neighbors - 1);
-			clustering_coefficient[i] = count / full_edges;
-			average += clustering_coefficient[i] / NUM_NODES;
-		}
-		//output for clustering coefficient
-		node_info << connectivity << endl;
-		for (int i = 0; i < NUM_NODES; i++)
-		{
-			node_info << clustering_coefficient[i] << " ";
-		}
-		node_info << endl;
-		general_info << connectivity << " " << average << endl;
+			node_info << endl;
+			general_info << connectivity << " " << average << endl;
 
-		free(graph);
+			free(graph);
+		}
 	}
-
 	return 0;
 }
